df96219a1d4f21d4781cdd6a8f06243c7c8e990f
nacha <nacha@nacha-pc>
2021-01-27 07:49:36 +1300

    intial

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -23,54 +23,27 @@ public class Counter {
 
 	// TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
-		do {
-			_ctr++;
-		} while (_ctr % 2 != 0);
+		_ctr =-99;
 	}
 
 	// TODO: dev1- method for decrement to closest even number
 	public void decrementToEven() {
-		do {
-			_ctr--;
-		} while (_ctr % 2 != 0);
+		_ctr =-99;
 	}
 
 	// TODO: dev2- method for increment to closest prime number
 	public void incrementToPrime() {
-		do {
-			_ctr++;
-		} while (isPrimeNumber(_ctr) );
+		_ctr =-99;
 	}
 
 	// TODO: dev2- method for decrement to closest prime number
 	public void decrementToPrime() {
-		do {
-			_ctr--;
-		} while (isPrimeNumber(_ctr));
+		_ctr =-99;
 	}
 
 	// TODO: dev3- count the frequency of word in sentence
 	public void countFrequency(String word, String sentence) {
-		reset();
-		StringTokenizer st = new StringTokenizer(sentence);
-		while(st.hasMoreTokens()) {
-			if(word.equals(st.nextToken())) {
-				_ctr++;
-			}
-		}
-	}
-
-	private boolean isPrimeNumber(int num) {
-		int i =2;
-		while (i <= num / 2) {
-			if (num % i == 0) {
-				return true;
-			}
-
-			i++;
-		}
-		return false;
-
+		_ctr =-99;
 	}
 
 }
